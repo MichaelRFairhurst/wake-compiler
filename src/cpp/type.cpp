3a90ae054ba53f4ee4dcd0ed63e35336bf4430ab
Mike Fairhurst <michael@manifestwebdesign.com>
2014-10-22 12:02:03 -0700

    Addinng docbloc headers

--- src/cpp/type.cpp
+++ src/cpp/type.cpp
@@ -1,3 +1,17 @@
+/**************************************************
+ * Source Code for the Original Compiler for the
+ * Programming Language Wake
+ *
+ * type.cpp
+ *
+ * Licensed under the MIT license
+ * See LICENSE.TXT for details
+ *
+ * Author: Michael Fairhurst
+ * Revised By:
+ *
+ **************************************************/
+
 #include "type.h"
 #include <cstddef>
 #include <cstdlib>
