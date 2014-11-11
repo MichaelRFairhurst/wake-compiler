830111d829ad304bb387a26290050df68d9fd5e1
Mike Fairhurst <michael@manifestwebdesign.com>
2014-11-11 15:02:27 -0800

    Test failure is from failure bailout, two separate tests required.

--- src/cpp/test/ParseTreeTraverserTest.cpp
+++ src/cpp/test/ParseTreeTraverserTest.cpp
@@ -3227,6 +3227,26 @@ PTT_TEST_CASE(
 );
 
 PTT_TEST_CASE(
+	TestAnnotatedMethodsClassesParseAndCheckSemantics,
+	"@Annotated													\n\
+	@AnnotatedVal(true)											\n\
+	@AnnotatedParams('test', 123)								\n\
+	every MyClass is:											\n\
+		@Annotated												\n\
+		@AnnotatedVal(true)										\n\
+		@AnnotatedParams('test', 123)							\n\
+		myMethod() {											\n\
+		}														\n\
+																\n\
+		@Annotated												\n\
+		@AnnotatedVal(true)										\n\
+		@AnnotatedParams('test', 123)							\n\
+		myMethod() {											\n\
+		}",
+	PTT_EXPECT(MULTIPLE_METHOD_DEFINITION)
+);
+
+PTT_TEST_CASE(
 	TestAnnotatedMethodsNeedsClassesParseAndTypeCheck,
 	"@Annotated													\n\
 	@AnnotatedVal(true)											\n\
@@ -3242,22 +3262,9 @@ PTT_TEST_CASE(
 		@AnnotatedVal(true)										\n\
 		@AnnotatedParams('test', 123)							\n\
 		myTypecheckedMethod() {									\n\
-			1 + 'test';											\n\
-		}														\n\
-																\n\
-		@Annotated												\n\
-		@AnnotatedVal(true)										\n\
-		@AnnotatedParams('test', 123)							\n\
-		myMethod() {											\n\
-		}														\n\
-																\n\
-		@Annotated												\n\
-		@AnnotatedVal(true)										\n\
-		@AnnotatedParams('test', 123)							\n\
-		myMethod() {											\n\
+			Num + 'test';											\n\
 		}",
 	PTT_EXPECT(TYPE_ERROR)
-	PTT_EXPECT(MULTIPLE_METHOD_DEFINITION)
 );
 
 
