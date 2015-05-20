5b3c3c0c9af6e0a5886280765e224f7552a729f3
Mike Fairhurst <michaelrfairhurst@gmail.com>
2015-05-20 12:48:38 -0700

    Update brew formula

--- WakeFormula.rb
+++ WakeFormula.rb
@@ -3,7 +3,7 @@ require "formula"
 class Wake < Formula
   url "https://github.com/MichaelRFairhurst/wake-compiler/archive/v0.2.0.tar.gz"
   homepage "http://www.wakelang.com"
-  sha1 "36d3510f1e75bf64af4ce3af3c2b360bd503b9f8"
+  sha1 "0fefebc5c5c28f2adcabc44cc908155d2c655fd3"
 
   depends_on "boost" => 'c++11'
   depends_on "flex"
