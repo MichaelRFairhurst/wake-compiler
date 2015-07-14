e0b5b04545d7d4beb9b9d5719386fa43752c873c
Mike Fairhurst <michaelrfairhurst@gmail.com>
2015-07-13 20:42:57 -0700

    Use a manual release to get nested modules

--- WakeFormula.rb
+++ WakeFormula.rb
@@ -1,9 +1,9 @@
 require "formula"
 
 class Wake < Formula
-  url "https://github.com/MichaelRFairhurst/wake-compiler/archive/v0.2.1.tar.gz"
+  url "https://github.com/MichaelRFairhurst/wake-compiler/releases/download/v0.2.1/v0.2.1.tar.gz"
   homepage "http://www.wakelang.com"
-  sha1 "04920699e52f87e73f34f1a435f49bc3cbed9166"
+  sha1 "ee1a14e2c84ce547d68d62cb0da51af3defdf9c7"
 
   depends_on "boost" => 'c++11'
   depends_on "flex"
